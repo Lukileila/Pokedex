824afffdc279ef1aa64dd4717158f29c77c35c1c
Lukileila <142001977+Lukileila@users.noreply.github.com>
2023-10-31 18:15:33 +0100

    get requests set up, unsanitized

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,20 @@
-import express from 'express';
-import cors from 'cors';
-
+const cors = require('cors');
+const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
 
+const allPokemon = require('./pokedex.json');
 
-app.use(express.json());
-app.use(cors({ origin: '*' }));
+//Middlewares:
+app.use(cors()); // sets origin to all by default and thus enables Cross-Origin Resource Sharing (CORS)
+app.use(express.json()); 
 
-app.route('/').get((req, res) => {
-  console.log('Hello World'); 
-  return res.json({ message: 'Hello, World!' }); 
-});
+app.route('/pokemon').get((req, res) => { return res.json(allPokemon) });
 
-const port = process.env.PORT || 7000;
+app.route('/pokemon/:id').get((req, res) => {
+  const {id}=req.params;
+  return res.json(allPokemon[id-1])});
+
 app.listen(port, () =>
- console.log(`Server listening on port ${port}!`)
+  console.log(`Server listening on port ${port}!`)
 );

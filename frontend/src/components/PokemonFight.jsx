045e15b6828bbc0885d3967687ce7ca72cc760db
Catia <wolfgangcatia@gmail.com>
2023-11-08 11:59:13 +0100

    trying to add custom assets

--- frontend/src/components/PokemonFight.jsx
+++ frontend/src/components/PokemonFight.jsx
@@ -8,7 +8,7 @@ const Pokemon = (props) => {
     <div className="bg-white rounded p-4 shadow-lg">
       <h1 className="text-xl font-semibold">{name.toUpperCase()}</h1>
       <img src={image} alt={name} className="w-full h-auto mt-2" />
-      <h5>Type: ${type}</h5>
+      <h5>Type: {type}</h5>
       <h3 className="text-lg mt-2">Stats</h3>
       <div className="flex space-x-4 mt-2">
         <span className="text-sm">Attack: {attack}</span>

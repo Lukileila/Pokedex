a25056fe430b8342dceb0b415c5be8d48ea8773f
Lukileila <142001977+Lukileila@users.noreply.github.com>
2023-11-09 14:01:34 +0100

    basic setup done, not working yet, debugging

---
+++ frontend/src/components/Layout.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+export default function Layout(){    
+    return (
+    <>
+        <Navbar />
+        {children}
+        <Footer />
+       
+    </>
+    )
+}
---
+++ frontend/src/pages/Pokedex.jsx
@@ -0,0 +1,9 @@
+export default function Pokedex(){    
+    return (
+    <>
+    <div>
+    <p>Here should be a Pokedex.</p>
+    </div>
+    </>
+    )
+}
